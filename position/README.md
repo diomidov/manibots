4618df0f284f73c3ba695e178f46d24536c3c61d
Yevhenii Diomidov <diomidov@mit.edu>
2022-10-24 21:57:08 -0400

    Pos improvements

--- position/README.md
+++ position/README.md
@@ -1,1 +1,3 @@
-The average binary market on Manifold resolves to about 42%. This bot looks for new markets with no bets and buys NO.
+The average binary market on Manifold resolves to about 40%-43%. This bot looks for new markets with 0 bets and buys NO.
+
+The bot ignores markets which contain blacklisted words and phrases such as `@pos` and `this market`.
--- position/bot.py
+++ position/bot.py
@@ -1,9 +1,12 @@
 from manifoldpy import api as mf
 from time import sleep
 from collections import defaultdict
+from traceback import print_exception
+
 from secret_config import API_KEY
 
 USER_ID = "3S5gAHLCa3QgEAtC2D329hPPLY12"
+BLACKLIST = ['@pos', 'position', 'amplified odds', 'this market', 'resolves', 'conditional']
 
 def average_resolution(markets, include_prob=True):
     total = 0
@@ -23,6 +26,7 @@ def average_resolution(markets, include_prob=True):
 
 def print_stats():
     markets = mf.get_all_markets()
+    markets = [m for m in markets if all(w not in m.question.lower() for w in BLACKLIST)]
     print('Average prob including PROB:', average_resolution(markets, True))
     print('Average prob excluding PROB:', average_resolution(markets, False))
 
@@ -34,7 +38,7 @@ creator_count = defaultdict(int)
 
 def bet(m):
     print(f'Betting')
-    mf.make_bet(API_KEY, 10, m.id, 'NO', limitProb=0.43)
+    mf.make_bet(API_KEY, 15, m.id, 'NO', limitProb=0.40)
     # mf.make_bet(API_KEY, 1, m.id, 'YES', limitProb=0.05)
 
 def process_market(m):
@@ -43,7 +47,7 @@ def process_market(m):
     processed_market_ids.add(m.id)
 
     m = mf.get_market(m.id)
-    if len(m.bets) > 0 or m.outcomeType != 'BINARY':
+    if len(m.bets) > 0 or m.outcomeType != 'BINARY' or m.probability != 0.5:
         return
 
     print(f'New market: {m.question}')
@@ -53,7 +57,7 @@ def process_market(m):
         print(f'Too many markets from {m.creatorUsername}.')
         return
 
-    for word in ['@pos', 'position', 'amplified odds', 'this market', 'resolves', 'conditional']:
+    for word in BLACKLIST:
         if word in m.question.lower():
             print(f'Contains blacklisted word {word}')
             return
@@ -61,12 +65,13 @@ def process_market(m):
     bet(m)
 
 def main():
+    # print_stats()
     while True:
         for m in mf.get_markets(5):
             try:
                 process_market(m)
             except Exception as e:
-                print(e)
+                print_exception(e)
         sleep(0.25)
 
 if __name__ == '__main__':
